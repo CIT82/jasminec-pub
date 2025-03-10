50332a6ff5e622d54847148d9b2dfbbd7d46d220
Jasmine Chang <jailiahc13@gmail.com>
2025-03-09 18:35:08 -0700

    updated template

--- p0/assets/js/contact-me.js
+++ p0/assets/js/contact-me.js
@@ -4,12 +4,12 @@ const SENT = document.getElementById("sent")
 
 FORM.addEventListener('submit', function(e){
     e.preventDefault()
-    const name = e.target.name.value
-    const email = e.target.email.value
-    const subject = e.target.subject.value
-    const message = e.target.message.value
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const subject = e.target.subject.value;
+    const message = e.target.message.value;
     const p = document.getElementById("p")
-    p.textContent = `hello ${name} thanks for submitting your message ${message} !`
+    p.textContent = `${name}, Thank you for submitting your message on ${subject}!`
     SENT.appendChild(p)
     FORM.reset()
 })
